5fb4d6096cba9f8c5c70107d5a470f4c00ac2800
Michal Poradzinski <michal.poradzinski@ukaea.uk>
2022-11-09 15:40:30 +0000

    added robustness tests to patient normalize

--- tests/test_models.py
+++ tests/test_models.py
@@ -151,6 +151,21 @@ def test_daily_min_string():
             [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
             None,
         ),
+        (
+            'hello',
+            None,
+            TypeError,
+        ),
+        (
+            3,
+            None,
+            TypeError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        )
     ])
 def test_patient_normalise(test, expected, expect_raises):
     """Test normalisation works for arrays of one and positive integers."""
